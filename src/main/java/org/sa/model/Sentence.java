0202596601df9b04b0203d593a3e76a81d65f932
sikorric <richard.sikora@tietoevry.com>
2022-02-02 18:51:59 +0100

    Implement basic Sentence logic

--- src/main/java/org/sa/model/Sentence.java
+++ src/main/java/org/sa/model/Sentence.java
@@ -1,4 +1,42 @@
 package org.sa.model;
 
-public class Sentence {
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public final class Sentence {
+    private final String[] words;
+
+    public Sentence(final String sentence) {
+        Objects.requireNonNull(sentence, "sentence can not be null");
+        words =  Arrays.stream(sentence.split("\\s+"))
+                .map(String::trim)
+                .filter(Predicate.not(String::isEmpty))
+                .sorted(String::compareToIgnoreCase)
+                .toArray(String[]::new);
+    }
+
+    public String[] words() {
+        return Arrays.copyOf(words, words.length);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sentence sentence = (Sentence) o;
+        return Arrays.equals(words, sentence.words);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(words);
+    }
+
+    @Override
+    public String toString() {
+        return "Sentence{" +
+                "words=" + Arrays.toString(words) +
+                '}';
+    }
 }
---
+++ src/test/java/org/sa/model/SentenceTest.java
@@ -0,0 +1,31 @@
+package org.sa.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SentenceTest {
+
+    @Test
+    void shouldSortSentenceWordsIgnoreCase() {
+        assertArrayEquals(List.of("a", "A", "B", "b").toArray(), new Sentence("a B A b").words());
+    }
+
+    @Test
+    void shouldCreateOneWordSentence() {
+        assertArrayEquals(List.of("a").toArray(), new Sentence("a").words());
+    }
+
+    @Test
+    void shouldCreateEmptySentence() {
+        assertEquals(0, new Sentence("").words().length);
+        assertEquals(0, new Sentence(" \t\r\n").words().length);
+    }
+
+    @Test
+    void shouldFailOnNullSentence() {
+        assertThrows(NullPointerException.class, () -> new Sentence(null));
+    }
+}
