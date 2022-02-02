bcbbcdacea1ff6bb2f2404b9678e17fab325387f
sikorric <richard.sikora@tietoevry.com>
2022-02-02 15:27:16 +0100

    Add sentence scanner

---
+++ src/main/java/org/sa/SentenceScanner.java
@@ -0,0 +1,32 @@
+package org.sa;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class SentenceScanner implements AutoCloseable {
+    private static final Pattern SENTENCE_DELIMITER = Pattern.compile("(?<!Mr|Ms|Mrs)[.!?]");
+    private final Scanner scanner;
+
+    public SentenceScanner(InputStream stream) {
+        Objects.requireNonNull(stream, "input stream must not be null");
+        scanner = new Scanner(stream, StandardCharsets.UTF_8.name()).useDelimiter(SENTENCE_DELIMITER);
+    }
+
+    @Override
+    public void close() throws IOException {
+        scanner.close();
+    }
+
+    public Stream<String> stream() {
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(scanner, Spliterator.ORDERED), false)
+                .map(String::trim)
+                .filter(Predicate.not(String::isEmpty));
+    }
+}
---
+++ src/test/java/org/sa/SentenceScannerTest.java
@@ -0,0 +1,46 @@
+package org.sa;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SentenceScannerTest {
+    @Test
+    void shouldReturnSentences() {
+        assertEquals(List.of("a"), listOf(scannerFor("a")));
+        assertEquals(List.of("a", "b", "c", "d"), listOf(scannerFor("a.b?c!d.")));
+    }
+
+    @Test
+    void shouldSkipEmptySentences() {
+        assertTrue(listOf(scannerFor("")).isEmpty());
+        assertTrue(listOf(scannerFor(".")).isEmpty());
+        assertTrue(listOf(scannerFor("..")).isEmpty());
+    }
+
+    @Test
+    void shouldRecognizeCommonAbbreviations() {
+        assertEquals(1, scannerFor("Mr. Ms. Mrs.").stream().count());
+    }
+
+    @Test
+    void shouldScanSampleFile() throws FileNotFoundException {
+        final var scanner = new SentenceScanner(new FileInputStream(Path.of("src/test/resources/sample-files/small.in").toFile()));
+        assertEquals(13, scanner.stream().count());
+    }
+
+    private static SentenceScanner scannerFor(String text) {
+        return new SentenceScanner(new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8)));
+    }
+    private static List<String> listOf(SentenceScanner scanner) {
+        return scanner.stream().collect(Collectors.toList());
+    }
+}
