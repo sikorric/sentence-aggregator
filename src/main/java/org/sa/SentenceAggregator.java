61e86ae122e5d75f05afa850b52ad26071c5dd17
sikorric <richard.sikora@tietoevry.com>
2022-02-02 18:53:22 +0100

    Wire the application and add usage information

--- src/main/java/org/sa/SentenceAggregator.java
+++ src/main/java/org/sa/SentenceAggregator.java
@@ -1,7 +1,31 @@
 package org.sa;
 
+import org.sa.io.CsvSentenceWriter;
+import org.sa.io.SentenceScanner;
+import org.sa.io.SentenceWriter;
+import org.sa.io.XmlSentenceWriter;
+import org.sa.model.Sentence;
+
 public class SentenceAggregator {
     public static void main(String[] args) {
-        System.out.println("Sentence Aggregator!");
+        if (args.length != 1 || (!"--xml".equals(args[0]) && !"--csv".equals(args[0]))) {
+            System.out.println("usage: java -jar sentence-aggregator.jar --csv|--xml");
+            System.out.println("\nThe application reads sentences delimited by [.?!] from the standard \n" +
+                    "input and writes ordered sentence words to the standard output.");
+            System.out.println("\nOptions:\n" +
+                    "  --xml  write output in XML format\n" +
+                    "  --csv  write output in CSV format");
+            System.exit(1);
+        }
+        try (SentenceWriter writer = "--xml".equals(args[0])
+                ? new XmlSentenceWriter()
+                : new CsvSentenceWriter()) {
+
+            new SentenceScanner(System.in).stream()
+                    .map(Sentence::new)
+                    .forEach(writer::write);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
